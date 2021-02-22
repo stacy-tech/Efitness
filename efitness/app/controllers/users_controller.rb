16449158ff4fbc83d4087a0e5f906d7ba8e97246
stacy <stacydn23@gmail.com>
2021-02-21 21:18:16 -0500

    add secret sessions, and build the signup route

--- efitness/app/controllers/users_controller.rb
+++ efitness/app/controllers/users_controller.rb
@@ -1,37 +1,17 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get '/signup' do
+    erb :'users/signup.html'
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
-  end
-
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
-
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
+  post '/signup' do
+    user = User.create(params)
+    if user.valid?
+      flash[:success]="sucessfully created a user"
+      redirect '/login'
+    else
+      flash[:error]=user.errors.full_messages.to_sentence
+      redirect '/signup'
+    end
   end
 end
