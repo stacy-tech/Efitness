5b8990a704f53921af54248f2eed7a29ee99bb82
stacy <stacydn23@gmail.com>
2021-02-22 00:27:42 -0500

    contollers updated

--- efitness/app/controllers/efits_controller.rb
+++ efitness/app/controllers/efits_controller.rb
@@ -1,37 +1,7 @@
 class EfitsController < ApplicationController
 
-  # GET: /efits
-  get "/efits" do
-    erb :"/efits/index.html"
+  get '/efits' do
+    erb :'efits/index.html'
   end
 
-  # GET: /efits/new
-  get "/efits/new" do
-    erb :"/efits/new.html"
-  end
-
-  # POST: /efits
-  post "/efits" do
-    redirect "/efits"
-  end
-
-  # GET: /efits/5
-  get "/efits/:id" do
-    erb :"/efits/show.html"
-  end
-
-  # GET: /efits/5/edit
-  get "/efits/:id/edit" do
-    erb :"/efits/edit.html"
-  end
-
-  # PATCH: /efits/5
-  patch "/efits/:id" do
-    redirect "/efits/:id"
-  end
-
-  # DELETE: /efits/5/delete
-  delete "/efits/:id/delete" do
-    redirect "/efits"
-  end
 end
