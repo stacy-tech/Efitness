cae04b9331637828201162e465eacfb2d9b1d614
stacy <stacydn23@gmail.com>
2021-02-21 19:33:50 -0500

    model class structure

--- efitness/app/models/efit.rb
+++ efitness/app/models/efit.rb
@@ -1,2 +1,4 @@
 class Efit < ActiveRecord::Base
+    belongs_to :user
+    validates :name, uniqueness: true, presence: true
 end
--- efitness/app/models/user.rb
+++ efitness/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :efits
+    validates :username, :email, presence: true, uniqueness: true
 end
