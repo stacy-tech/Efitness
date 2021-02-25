e71312cb50f3461b167ab49a91e6058945d153e1
stacy <stacydn23@gmail.com>
2021-02-24 22:42:10 -0500

    structure workouts id route

--- efitness/app/controllers/workout_controller.rb
+++ efitness/app/controllers/workout_controller.rb
@@ -14,8 +14,8 @@ class WorkoutsController < ApplicationController
 
   get '/workouts/:id' do
     redirect_if_not_logged_in
-    @workouts = Workouts.find(params[:id])
-    if current_user == @workouts.user && @workouts
+    @workout = Workout.find(params[:id])
+    if current_user == @workout.user
       erb :'workouts/show.html'
     else
       # flash[:error]="Authorization denied"
@@ -24,21 +24,21 @@ class WorkoutsController < ApplicationController
     
   end
 
-  # get '/workouts/:id/edit' do 
-  #   redirect_if_not_logged_in
-  #   @workouts = Workouts.find(params[:id])
-  #   if current_user == @workouts.user && @workouts
-  #   erb :'workouts/edit.html'
-  #   else
-  #     redirect '/login'
-  #   end
-  # end
+  get '/workouts/:id/edit' do 
+    redirect_if_not_logged_in
+    @workout = Workout.find(params[:id])
+    if current_user == @workout.user
+    erb :'workouts/edit.html'
+    else
+      redirect '/workouts'
+    end
+  end
 
   post '/workouts' do
     redirect_if_not_logged_in
     workout = current_user.workouts.build(params["workout"])
     params[:exercises].each do |hash|
-      if  hash[:name] != nil && hash[:name] != ""
+      if  hash[:name] != "nil" && hash[:name] != ""
         exercise = Exercise.find_or_create_by(name: hash["name"])
         exercise.bodypart = hash["bodypart"] if hash["bodypart"] && hash["bodypart"] != ""
         exercise.save
@@ -48,7 +48,7 @@ class WorkoutsController < ApplicationController
     end
     if workout.save
       # flash[:sucess]="Successfully save new workout." 
-      redirect to '/workouts/show.html'
+      redirect to "/workouts/#{workout.id}"
     else
       # flash[:error]= workout.errors.full_messages.to_sentence
       redirect to '/workouts/new'
